48cd3269c1a4240d40e57707d4d1de22cf04c56d
Walker Sorlie <walkersorlie@gmail.com>
2020-09-22 15:10:15 -0700

    More TableModel implementation

--- src/main/java/com/walkersorlie/lolchampioninfo/ChampionTableModel.java
+++ src/main/java/com/walkersorlie/lolchampioninfo/ChampionTableModel.java
@@ -12,12 +12,23 @@ import javax.swing.table.AbstractTableModel;
 public class ChampionTableModel extends AbstractTableModel {
 
     private Champion champion;
-    private Object[][] data;
-    private String[] columnNames =  {"Stat Name", "Value"};
-
+    private Object[][] championData;
+    private final String[] columnNames =  {"Champion", "Value"};
+    private final String[] rowNames = {"ID", "Key", "Name", "Ally tips", "Enemy tips", "Stats", "Spells", "Passive"}
 
     public ChampionTableModel(Champion champion) {
         this.champion = champion;
+        
+        championData = {
+            { champion.getID() },
+            { champion.getKey() },
+            { champion.getName() },
+            { champion.getAllyTips() },
+            { champion.getEnemyTips() },
+            { champion.getStats() },
+            { champion.getSpells() },
+            { champion.getPassive() }
+        }
     }
 
     @Override
@@ -29,14 +40,31 @@ public class ChampionTableModel extends AbstractTableModel {
     public int getColumnCount() {
         return columnNames.length;
     }
+    
+    public String getRowName(int row) {
+        return rowNames[row];
+    }
 
     @Override
     public int getRowCount() {
-        return champion.NUMBER_OF_FIELDS;
+        return Champion.NUMBER_OF_FIELDS;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        private ChampionStats stats;
+        private Map<String, ChampionSpell> spells;
+        private Map<String, String> passive;
+
+        if (rowIndex == 0 || rowIndex == 1 || rowIndex == 2) {
+            return championData[rowIndex][columnIndex]
+        }
+        else if (rowIndex == 3 || rowIndex == 4) {
+            String[] tips = championData[rowIndex][columnIndex].toArray();
+            return tips;
+        }
+        
+        return "None";
     }
 }
